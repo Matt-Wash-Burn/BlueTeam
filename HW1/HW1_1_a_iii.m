92f77a5a839f3ebbafac3920312528e53fa1bcba
Nick DeMarco <33133669+nickjdemarco@users.noreply.github.com>
2018-02-27 20:49:27 -0500

    Added part 3 of question 1a.

---
+++ HW1/HW1_1_a_iii.m
@@ -0,0 +1,55 @@
+% Author: Brandon Bench
+% Hybrid Image
+
+clc;
+clear;
+close all; % closes all figures
+
+%% Setup
+image1 = imread('dog.bmp');
+image2 = imread('einstein.bmp');
+image3 = imread('fish.bmp');
+
+image1double = double(image1)/255;
+image2double = double(image2)/255;
+image3double = double(image3)/255;
+
+im1 = rgb2gray(image1double);
+im2 = rgb2gray(image2double);
+im3 = rgb2gray(image3double);
+
+
+%% Applying the filters on input images
+im1_fft  = fft2(im1);
+im2_fft  = fft2(im2);
+im3_fft  = fft2(im3);
+
+%% Nuetralizing the Phase to display Magnitude only
+im1_M = abs(im1_fft);
+im2_M = abs(im2_fft);
+im3_M = abs(im3_fft);
+
+%% Inverse fft2
+restoredP1 = log(abs(ifft2(im1_M*exp(1i*0)))+1);
+restoredP2 = log(abs(ifft2(im2_M*exp(1i*0)))+1);
+restoredP3 = log(abs(ifft2(im3_M*exp(1i*0)))+1);
+
+%% Calculating plotting limits
+I_Mag_min = min(min(abs(restoredP1)));
+I_Mag_max = max(max(abs(restoredP1)));
+
+
+figure('position', [200, 200, 1000, 400]); subplot(1,2,1), imshow(image1), title("Fluffy")
+subplot(1,2,2), 
+imshow(abs(restoredP1),[I_Mag_min I_Mag_max ]);
+title("Dog Phase Nuetralized")
+
+figure('position', [200, 200, 1000, 400]); subplot(1,2,1), imshow(image2), title("Mr. Einstein")
+subplot(1,2,2), 
+imshow(abs(restoredP2),[I_Mag_min I_Mag_max ]);
+title("Albert Phase Nuetralized")
+
+figure('position', [200, 200, 1000, 400]); subplot(1,2,1), imshow(image3), title("Pescado")
+subplot(1,2,2), 
+imshow(abs(restoredP3),[I_Mag_min I_Mag_max ]);
+title("Fish Phase Nuetralized")

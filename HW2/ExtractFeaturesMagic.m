aa34774795aa1e8841d7bcc6bee45a961978a12b
Matt-Wash-Burn <mattwburn@gmail.com>
2018-03-28 16:18:21 -0400

    Move in .mat files but Ignored

---
+++ HW2/ExtractFeaturesMagic.m
@@ -0,0 +1,53 @@
+function stuff = ExtractFeaturesMagic(trainingPixels)
+%ExtractFeaturesMagic Extracts the features of the row vector using the
+%magnatude components of the fft 
+%   Detailed explanation goes here
+
+fim = reshape(trainingPixels, [48,48])'; % row = 48 x 48  image
+% imagesc(fim)                                       % show the image
+% title(num2str(trainingPixels))               % show the label
+
+
+
+% imagesc(sfim); 
+
+%% Frequency componenets from Nick submission 
+
+%% Applying the filters on input images
+
+wname = 'haar';
+[cA1,cH1,cV1,cD1] = dwt2(fim,wname);
+
+[cA2,cH2,cV2,cD2] = dwt2(cA1,wname);
+
+waveStuff1 = [cA1 cH1 ; cV1 cD1]; 
+waveStuff2 = [cA2 cH2 ; cV2 cD2]; 
+
+% figure; imagesc(waveStuff1); 
+% figure; imagesc(waveStuff2); 
+% 
+% 
+% tic
+% toc
+
+% 
+% %% Calculating plotting limits
+% I_Mag_min = min(min(abs(restoredP1)));
+% I_Mag_max = max(max(abs(restoredP1)));
+% 
+% % figure; 
+% % imshow(abs(re),[I_Mag_min I_Mag_max ]);
+% 
+% 
+% %% Extract lower frequencies by just cutting to 16 x 16 
+% newRe = re(5:20,5:20); 
+% % figure; imagesc(newRe); 
+% 
+% %% Reshape to return to NN 
+% 
+
+stuff = reshape(waveStuff2, [1,576]); 
+
+
+end
+
